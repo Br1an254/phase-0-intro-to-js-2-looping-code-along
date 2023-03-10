8622c99409fc92a2640691202e27ebbabf84fff3
Brian Babu <briansmkenya@gmail.com>
2023-03-11 02:45:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(stringArray, eventName) {
+    let messagesArray = [];
+
+    for(let i = 0; i < stringArray.length; i++) {
+        messagesArray.push(`Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return messagesArray
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(n) {
+    let i = n;
+
+    while(i >= 0) {
+      console.log(i)
+      i--
+    }
+}
+countDown(10)
